d8050229e78b74eca41b7ef443b05c4654bfb24f
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-08-27 17:29:55 -0700

    adding queries/ still working on query #5

--- Phase3/code_2/java/src/MechanicShop.java
+++ Phase3/code_2/java/src/MechanicShop.java
@@ -23,7 +23,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.*; //going to import random
 /**
  * This class defines a simple embedded SQL utility class that is designed to
  * work with PostgreSQL JDBC drivers.
@@ -34,14 +34,14 @@ public class MechanicShop{
 	//reference to physical database connection
 	private Connection _connection = null;
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	
+
 	public MechanicShop(String dbname, String dbport, String user, String passwd) throws SQLException {
 		System.out.print("Connecting to database...");
 		try{
 			// constructs the connection URL
 			String url = "jdbc:postgresql://localhost:" + dbport + "/" + dbname;
 			System.out.println ("Connection URL: " + url + "\n");
-			
+
 			// obtain a physical connection
 	        this._connection = DriverManager.getConnection(url, user, passwd);
 	        System.out.println("Done");
@@ -51,15 +51,15 @@ public class MechanicShop{
 	        System.exit(-1);
 		}
 	}
-	
+
 	/**
 	 * Method to execute an update SQL statement.  Update SQL instructions
 	 * includes CREATE, INSERT, UPDATE, DELETE, and DROP.
-	 * 
+	 *
 	 * @param sql the input SQL string
 	 * @throws java.sql.SQLException when update failed
 	 * */
-	public void executeUpdate (String sql) throws SQLException { 
+	public void executeUpdate (String sql) throws SQLException {
 		// creates a statement object
 		Statement stmt = this._connection.createStatement ();
 
@@ -74,7 +74,7 @@ public class MechanicShop{
 	 * Method to execute an input query SQL instruction (i.e. SELECT).  This
 	 * method issues the query to the DBMS and outputs the results to
 	 * standard out.
-	 * 
+	 *
 	 * @param query the input query string
 	 * @return the number of rows returned
 	 * @throws java.sql.SQLException when failed to execute the query
@@ -93,7 +93,7 @@ public class MechanicShop{
 		ResultSetMetaData rsmd = rs.getMetaData ();
 		int numCol = rsmd.getColumnCount ();
 		int rowCount = 0;
-		
+
 		//iterates through the result set and output them to standard out.
 		boolean outputHeader = true;
 		while (rs.next()){
@@ -112,48 +112,48 @@ public class MechanicShop{
 		stmt.close ();
 		return rowCount;
 	}
-	
+
 	/**
 	 * Method to execute an input query SQL instruction (i.e. SELECT).  This
 	 * method issues the query to the DBMS and returns the results as
 	 * a list of records. Each record in turn is a list of attribute values
-	 * 
+	 *
 	 * @param query the input query string
 	 * @return the query result as a list of records
 	 * @throws java.sql.SQLException when failed to execute the query
 	 */
-	public List<List<String>> executeQueryAndReturnResult (String query) throws SQLException { 
-		//creates a statement object 
-		Statement stmt = this._connection.createStatement (); 
-		
-		//issues the query instruction 
-		ResultSet rs = stmt.executeQuery (query); 
-	 
+	public List<List<String>> executeQueryAndReturnResult (String query) throws SQLException {
+		//creates a statement object
+		Statement stmt = this._connection.createStatement ();
+
+		//issues the query instruction
+		ResultSet rs = stmt.executeQuery (query);
+
 		/*
-		 * obtains the metadata object for the returned result set.  The metadata 
-		 * contains row and column info. 
-		*/ 
-		ResultSetMetaData rsmd = rs.getMetaData (); 
-		int numCol = rsmd.getColumnCount (); 
-		int rowCount = 0; 
-	 
-		//iterates through the result set and saves the data returned by the query. 
+		 * obtains the metadata object for the returned result set.  The metadata
+		 * contains row and column info.
+		*/
+		ResultSetMetaData rsmd = rs.getMetaData ();
+		int numCol = rsmd.getColumnCount ();
+		int rowCount = 0;
+
+		//iterates through the result set and saves the data returned by the query.
 		boolean outputHeader = false;
-		List<List<String>> result  = new ArrayList<List<String>>(); 
+		List<List<String>> result  = new ArrayList<List<String>>();
 		while (rs.next()){
-			List<String> record = new ArrayList<String>(); 
-			for (int i=1; i<=numCol; ++i) 
-				record.add(rs.getString (i)); 
-			result.add(record); 
-		}//end while 
-		stmt.close (); 
-		return result; 
+			List<String> record = new ArrayList<String>();
+			for (int i=1; i<=numCol; ++i)
+				record.add(rs.getString (i));
+			result.add(record);
+		}//end while
+		stmt.close ();
+		return result;
 	}//end executeQueryAndReturnResult
-	
+
 	/**
 	 * Method to execute an input query SQL instruction (i.e. SELECT).  This
 	 * method issues the query to the DBMS and returns the number of results
-	 * 
+	 *
 	 * @param query the input query string
 	 * @return the number of rows returned
 	 * @throws java.sql.SQLException when failed to execute the query
@@ -174,20 +174,20 @@ public class MechanicShop{
 		stmt.close ();
 		return rowCount;
 	}
-	
+
 	/**
 	 * Method to fetch the last value from sequence. This
-	 * method issues the query to the DBMS and returns the current 
+	 * method issues the query to the DBMS and returns the current
 	 * value of sequence used for autogenerated keys
-	 * 
+	 *
 	 * @param sequence name of the DB sequence
 	 * @return current value of a sequence
 	 * @throws java.sql.SQLException when failed to execute the query
 	 */
-	
+
 	public int getCurrSeqVal(String sequence) throws SQLException {
 		Statement stmt = this._connection.createStatement ();
-		
+
 		ResultSet rs = stmt.executeQuery (String.format("Select currval('%s')", sequence));
 		if (rs.next()) return rs.getInt(1);
 		return -1;
@@ -208,7 +208,7 @@ public class MechanicShop{
 
 	/**
 	 * The main execution method
-	 * 
+	 *
 	 * @param args the command line arguments this inclues the <mysql|pgsql> <login file>
 	 */
 	public static void main (String[] args) {
@@ -218,12 +218,12 @@ public class MechanicShop{
 		            " <dbname> <port> <user>");
 			return;
 		}//end if
-		
+
 		MechanicShop esql = null;
-		
+
 		try{
 			System.out.println("(1)");
-			
+
 			try {
 				Class.forName("org.postgresql.Driver");
 			}catch(Exception e){
@@ -232,14 +232,14 @@ public class MechanicShop{
 				e.printStackTrace();
 				return;
 			}
-			
+
 			System.out.println("(2)");
 			String dbname = args[0];
 			String dbport = args[1];
 			String user = args[2];
-			
+
 			esql = new MechanicShop (dbname, dbport, user, "");
-			
+
 			boolean keepon = true;
 			while(keepon){
 				System.out.println("MAIN MENU");
@@ -255,7 +255,7 @@ public class MechanicShop{
 				System.out.println("9. ListKCarsWithTheMostServices");
 				System.out.println("10. ListCustomersInDescendingOrderOfTheirTotalBill");
 				System.out.println("11. < EXIT");
-				
+
 				/*
 				 * FOLLOW THE SPECIFICATION IN THE PROJECT DESCRIPTION
 				 */
@@ -281,7 +281,7 @@ public class MechanicShop{
 					System.out.print("Disconnecting from database...");
 					esql.cleanup ();
 					System.out.println("Done\n\nBye !");
-				}//end if				
+				}//end if
 			}catch(Exception e){
 				// ignored.
 			}
@@ -303,61 +303,139 @@ public class MechanicShop{
 		}while (true);
 		return input;
 	}//end readChoice
-	
+
 	public static void AddCustomer(MechanicShop esql)
         {//1
-	    try{
-		String query = "SELECT * FROM Catalog WHERE cost < ";
 
-		System.out.print("\tEnter cost: $");
-		String input = in.readLine();
-		query += input;
+	  //   try{
+		// String query = "SELECT * FROM Catalog WHERE cost < ";
+		//
+		// System.out.print("\tEnter cost: $");
+		// String input = in.readLine();
+		// query += input;
+		//
+		// int rowCount = esql.executeQuery (query);
+		// System.out.println ("total row(s): " + rowCount);
+		//
+	  //   }catch(Exception e)
+	  //   {
+		// System.err.println (e.getMessage ());
+	  //   }
+		try{
+		      int id_i  = Integer.parseInt(esql.executeQueryAndReturnResult("SELECT MAX(id) FROM customer;").get(0).get(0)) + 1;
+					System.out.print("\tnext id: " + idIn +"\n");
+					System.out.print("\tEnter fname: $");
+					String fname_i = in.readLine();
+					//System.out.print(fname_i);
+					System.out.print("\tEnter lname: $");
+					String lname_i = in.readLine();
+					//System.out.print(lname_i);
+					System.out.print("\tEnter phone no.: $");
+					String phone_i = in.readLine();
+					//System.out.print(phone_i);
+					System.out.print("\tEnter address : $");
+					String address_i = in.readLine();
+					//System.out.print(address_i);
+					String sql = "INSERT INTO customer VALUES ('" + id_i + "', '" + fname_i + "', '" + lname_i + "', '" + phone_i + "', '" + address_i + "');";
+					esql.executeUpdate(sql);}
+					catch(Exception e)
+					{
+						System.err.println(e.getMessage());
+					}
 
-		int rowCount = esql.executeQuery (query);
-		System.out.println ("total row(s): " + rowCount);
-        
-	    }catch(Exception e)
-	    {
-		System.err.println (e.getMessage ());
-	    }		
 	}
-	
+
 	public static void AddMechanic(MechanicShop esql){//2
-		
+		try{
+		      int id_i  = Integer.parseInt(esql.executeQueryAndReturnResult("SELECT MAX(id) FROM mechanic;").get(0).get(0)) + 1;
+					System.out.print("\tnext id: " + idIn +"\n");
+					System.out.print("\tEnter fname: $");
+					String fname_i = in.readLine();
+					//System.out.print(fname_i);
+					System.out.print("\tEnter lname: $");
+					String lname_i = in.readLine();
+					//System.out.print(lname_i);
+					System.out.print("\tEnter experience: $");
+					String experience_i = in.readLine();
+					//System.out.print(experience_i);
+					String sql = "INSERT INTO mechanic VALUES ('" + id_i + "', '" + fname_i + "', '" + lname_i + "', '" + experience_i + "');";
+					esql.executeUpdate(sql);}
+					catch(Exception e)
+					{
+						System.err.println(e.getMessage());
+					}
+
+
 	}
-	
+
 	public static void AddCar(MechanicShop esql){//3
-		
+		System.out.println("What is the VIN of your car?:");
+		String vin = in.readLine();
+
+
+
+
 	}
-	
+
 	public static void InsertServiceRequest(MechanicShop esql){//4
-		
+
 	}
-	
+
 	public static void CloseServiceRequest(MechanicShop esql) throws Exception{//5
-		
+		 System.out.print("\tEnter rid: $");
+		 String rid_i= in.readLine();
+		 String q1= "SELECT * FROM service_request WHERE rid = '"+rid_i+"';";
+		 String q2 = "SELECT * FROM closed_request WHERE wid = '"+rid_i+"';";
+		 System.out.print("\tEnter mid: $");
+			String mid_i = in.readLine();
+			String q3 = "SELECT * FROM mechanic WHERE id = '"+mid_i+"';";
+			System.out.print("\tEnter date: $");
+    	String date_i = in.readLine();
+			System.out.print("\tEnter comment: $");
+			String comment_i = in.readLine();
+			System.out.print("\tEnter bill: $");
+			String bill_i = in.readLine();
+
+			int a = esql.executeQuery(q1);
+			int b = esql.executeQuery(q2);
+			int c = esql.executeQuery(q3);
+
+			if(a ==1)
+			{
+				if (b==0)
+				{
+					if (c==1)
+					{
+				    String s = "INSERT INTO closed_request VALUES('"		
+					}
+				}
+			}
+
+
+
+
 	}
-	
+
 	public static void ListCustomersWithBillLessThan100(MechanicShop esql){//6
-		
+
 	}
-	
+
 	public static void ListCustomersWithMoreThan20Cars(MechanicShop esql){//7
-		
+
 	}
-	
+
 	public static void ListCarsBefore1995With50000Milles(MechanicShop esql){//8
-		
+
 	}
-	
+
 	public static void ListKCarsWithTheMostServices(MechanicShop esql){//9
 		//
-		
+
 	}
-	
+
 	public static void ListCustomersInDescendingOrderOfTheirTotalBill(MechanicShop esql){//9
 		//
-		
+
 	}
-	
+
 }
